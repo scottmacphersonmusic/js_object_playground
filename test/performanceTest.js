f30a197a1e2c17f3a094be284063ae3b9b66b13b
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-10-05 14:08:46 -0700

    BDD Performance#toString() method

--- test/performanceTest.js
+++ test/performanceTest.js
@@ -9,4 +9,12 @@ describe('A Performance instance', function() {
     expect(performance.date).to.be.a('date');
     expect(performance.compensation).to.equal(350);
   });
+
+  it('should have a toString() method', function() {
+    var date = new Date(2015, 10, 7, 20, 30);
+    var performance = new Performance('Paramount', date, 350);
+    expect(performance.toString()).to.equal(
+      "Paramount\n10/6/2015 @ 20:30\n$350\n"
+    );
+  });
 });
